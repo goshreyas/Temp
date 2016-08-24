49749f5fa59f74ffd5e60cc79f27562117361e86
Shreyas <shreyas.ranjan@gmail.com>
2016-08-24 03:59:45 -0700

    set_test.c

---
+++ set_test.c
@@ -0,0 +1,44 @@
+/*
+   4. Write a program to take 2 ararys (A and B) and output (A-B) U (B-A).
+   e.g. if A = {1, 3, 5}, B = {2, 3, 4, 5}, then (A-B) U (B-A) = {1, 2, 4}
+ */
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int set(){
+    int i=0, j=0, k=0, C[10];
+    int A[] = {1, 3, 6, 7, 8}, B[] = {2, 3, 6, 7}, m=5, n=4;
+    while(i<m && j<n) {
+        if(A[i] < B[j]){
+            C[k++] = A[i];
+            i++;            
+        } else if(A[i] > B[j]){
+            C[k++] = B[j];
+            j++;
+        }
+        else{i++; j++;}
+    }
+
+    if(i<m) {
+        for (;i<m; i++)
+            C[k++] = A[i];
+    }
+    else if(j<n) {
+        for (;j<n; j++)
+            C[k++] = B[j];
+    }
+
+    for(i=0; i<k; i++){
+        printf("%d, ", C[i]);
+    }
+    printf("\n");
+}
+
+void main() {
+    set();
+}
+
+
+
